344fee4654b3403f835fe15d1d96ecc60b5b8ddd
SebastianSalas <janiersalas09@gmail.com>
2021-10-19 21:25:07 -0500

    Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/basics'
   get 'static_pages/ruby'
   get 'static_pages/git'
-  root 'application#hello'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,20 +4,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Bootcamp Hogaru"
   end
 
   test "should get basics" do
     get static_pages_basics_url
     assert_response :success
+    assert_select "title", "Basics | Bootcamp Hogaru"
   end
 
   test "should get ruby" do
     get static_pages_ruby_url
     assert_response :success
+    assert_select "title", "Ruby | Bootcamp Hogaru"
   end
 
   test "should get git" do
     get static_pages_git_url
     assert_response :success
+    assert_select "title", "Git | Bootcamp Hogaru"
   end
 end
