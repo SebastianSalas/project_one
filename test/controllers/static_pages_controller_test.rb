c222873c50b8020baf44db0ae92f7df544f3f109
SebastianSalas <janiersalas09@gmail.com>
2021-10-20 00:18:14 -0500

    Prueba errores

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,27 +1,31 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Bootcamp Hogaru"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    #assert_select "title", "Home | Bootcamp Hogaru"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get basics" do
     get static_pages_basics_url
     assert_response :success
-    #assert_select "title", "Basics | Bootcamp Hogaru"
+    assert_select "title", "Basics | #{@base_title}"
   end
 
   test "should get ruby" do
     get static_pages_ruby_url
     assert_response :success
-    #assert_select "title", "Ruby | Bootcamp Hogaru"
+    assert_select "title", "Ruby | #{@base_title}"
   end
 
   test "should get git" do
     get static_pages_git_url
     assert_response :success
-    #assert_select "title", "Git | Bootcamp Hogaru"
+    assert_select "title", "Git | #{@base_title}"
   end
 end
